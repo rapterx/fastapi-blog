48ff1745f6067e1fd140ed1c7771846a464952d9
rapterx <dsudip330@gmail.com>
2025-04-30 20:14:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# FastApi Blog Site
+
+I showed how to use RESTful API  with FastApi. How to implement Middleware, Authentication, connect SQLAlchemy database and multi Routers. 
+
+
+## Acknowledgements
+
+ - [SQLAlchemy DataBase](https://github.com/Sinanoloji/fastapi-blog/tree/main/db)
+ - [Router](https://github.com/Sinanoloji/fastapi-blog/tree/main/router)
+ - [Authentication](https://github.com/Sinanoloji/fastapi-blog/tree/main/auth)
+
+
+## Start Project
+
+To activate Python env
+```bash
+  source env/bin/activate
+```
+Then to start this project run
+```bash
+  uvicorn main:app --reload
+```
+
---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from router import blog_get, blog_post, user, article, product
+from auth import authentication
+from exceptions import StoryException
+from db.database import engine
+from db import models
+
+
+app = FastAPI()
+app.include_router(blog_get.router)
+app.include_router(blog_post.router)
+app.include_router(user.router)
+app.include_router(article.router)
+app.include_router(product.router)
+app.include_router(authentication.router)
+
+@app.get("/hello")
+def index():
+    return {"message": "Hello World"}
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(
+        status_code=418,
+        content={'detail': exc.name}
+    )
+
+
+models.Base.metadata.create_all(engine)
+
+origins = [
+    'http://localhost:8000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
